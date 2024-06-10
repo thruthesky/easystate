0818c31d90c99c1f1f598277ea37e511d3f36aaf
JaeHo Song <thruthesky@gmail.com>
2024-06-10 15:40:45 +0800

    update pubspec

--- example/main.dart
+++ example/main.dart
@@ -1,21 +1,4 @@
-import 'dart:async';
-
-import 'package:easystate/easystate.dart';
 import 'package:flutter/material.dart';
-
-class CountModel extends EasyState {
-  int value = 0;
-  increase() {
-    value++;
-    update();
-  }
-}
-
-CountModel countModel;
-void main() {
-  countModel = CountModel();
-  runApp(MyApp());
-}
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,7 +10,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({super.key});
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -46,89 +29,13 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'EasyState Counter :',
             ),
-            EasyBuilder(
-              builder: (context, model) => Text(
-                model.value.toString(),
-              ),
-              model: countModel,
-            ),
-            StreamBuilder(
-              stream: countModel.stream,
-              initialData: countModel,
-              builder: (context, snapshot) => Text(
-                snapshot.data.value.toString(),
-              ),
-            ),
-            RaisedButton(
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) {
-                    return NewScreen();
-                  },
-                ),
-              ),
-              child: Text('New Screen'),
-            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => countModel.increase(),
+        onPressed: () => print('Increment'),
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class NewScreen extends StatefulWidget {
-  const NewScreen({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _NewScreenState createState() => _NewScreenState();
-}
-
-class _NewScreenState extends State<NewScreen> {
-  StreamSubscription stream;
-
-  @override
-  void initState() {
-    stream = countModel.stream.listen((value) => setState(() => null));
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    stream.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Saved Suggestions'),
-      ),
-      body: Column(
-        children: [
-          Text(countModel.value.toString()),
-          RaisedButton(
-            onPressed: () => setState(() => countModel.increase()),
-            child: Text('Increase with setState()'),
-          ),
-          Divider(),
-          Text(countModel.value.toString()),
-          RaisedButton(
-            onPressed: () => setState(() => countModel.increase()),
-            child: Text('Increase with stream listing & setState()'),
-          ),
-          RaisedButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text('Back'),
-          ),
-        ],
       ),
     );
   }
